35a46db1eec8a64b6ad964ec750370403661f030
neverland <chenjiahan.jait@bytedance.com>
2024-07-01 16:45:31 +0800

    chore: setup playground

--- playground/rsbuild.config.ts
+++ playground/rsbuild.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginMdx } from '../src';
 
 export default defineConfig({
-	plugins: [pluginMdx()],
+	plugins: [pluginReact(), pluginMdx()],
 });
